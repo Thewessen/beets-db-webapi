7f6c6daa532d5fe6d631bf109d816900591324aa
Bart Kleijngeld <bartkl@gmail.com>
2019-06-08 11:46:26 +0200

    added UpdateAlbum mutation (and root) to schema

--- beetsdbwebapi/schema.py
+++ beetsdbwebapi/schema.py
@@ -1,12 +1,53 @@
 import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType
 from beetsdbwebapi.models import Album as AlbumModel
-#from beetsdbwebapi.models import db_session
+from beetsdbwebapi.models import db_session
 
 
 class Album(SQLAlchemyObjectType):
     class Meta:
         model = AlbumModel
+
+
+class UpdateAlbum(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int()
+        name = graphene.String()
+        genre = graphene.String()
+        year = graphene.String()
+        album_artist = graphene.String()
+
+    success = graphene.Boolean()
+    album_before = graphene.Field(lambda: Album)
+    album = graphene.Field(lambda: Album)
+
+    def mutate(self,
+               info,
+               id,
+               name=None,
+               genre=None,
+               year=None,
+               album_artist=None):
+        q = Album.get_query(info)
+        album = q.filter(AlbumModel.id == id).first()
+        album_before = AlbumModel(id=id,
+                                  name=album.name,
+                                  genre=album.genre,
+                                  year=album.year,
+                                  album_artist=album.album_artist)
+        if name is not None:
+            album.name = name
+        if genre is not None:
+            album.genre = genre
+        if year is not None:
+            album.year = year
+        if album_artist is not None:
+            album.album_artist = album_artist
+        
+        db_session.add(album)
+        db_session.commit()
+        success = True
+        return UpdateAlbum(success=success, album_before=album_before, album=album)
 
 
 class Query(graphene.ObjectType):
@@ -19,5 +60,7 @@ class Query(graphene.ObjectType):
         else:
             return q.all()
 
+class Mutation(graphene.ObjectType):
+    album = UpdateAlbum.Field()
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
